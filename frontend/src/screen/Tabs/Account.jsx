9dcddec75396acb9007fa10c684b6372fb21f3c1
Huong <nguyenthihuong10.2.2002@gmail.com>
2023-06-17 21:38:46 +0700

    fix not scroll in account screen

--- frontend/src/screen/Tabs/Account.jsx
+++ frontend/src/screen/Tabs/Account.jsx
@@ -5,6 +5,7 @@ import useAuth from "../../hooks/useAuth";
 import useAxios from "../../hooks/useAxios";
 import useSocket from "../../hooks/useSocket";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function Account({ navigation }) {
     const [user, setUser] = React.useState({})
@@ -41,109 +42,111 @@ export default function Account({ navigation }) {
     }
 
     return (
-        <View className="flex-1 items-center bg-white">
-            <View className="w-full h-60">
-                <Image
-                    src={"https://projectmgtcoach.com/wp-content/uploads/2015/03/light-blue-backgrounds-for-websites.jpg"}
-                    className="object-cover w-full h-full overflow-hidden"
-                />
-            </View>
-            <View className="-mt-2 p-4 bg-white rounded-t-xl w-full max-w-s shadow-sm">
-                <Text className="pt-24 text-center mb-2 text-slate-900 text-2xl font-bold">{user.name}</Text>
-                <View className="m-auto space-x-5">
-                    <TouchableOpacity
-                        style={styles.bg}
-                        className="p-3 rounded-lg flex-row items-center justify-center w-fit"
-                        onPress={() => navigation.navigate("Chỉnh sửa thông tin", { user: user })}
-                    >
-                        <Feather name="edit" size={24} color={"white"} />
-                        <Text className="w-fit ml-3 font-bold text-white">Chỉnh sửa thông tin</Text>
-                    </TouchableOpacity>
-                    {/* <TouchableOpacity style={styles.bg} className="p-3 rounded-lg flex-row items-center justify-center w-40">
+        <ScrollView>
+            <View className="flex-1 items-center bg-white">
+                <View className="w-full h-60">
+                    <Image
+                        src={"https://projectmgtcoach.com/wp-content/uploads/2015/03/light-blue-backgrounds-for-websites.jpg"}
+                        className="object-cover w-full h-full overflow-hidden"
+                    />
+                </View>
+                <View className="-mt-2 p-4 bg-white rounded-t-xl w-full max-w-s shadow-sm">
+                    <Text className="pt-24 text-center mb-2 text-slate-900 text-2xl font-bold">{user.name}</Text>
+                    <View className="m-auto space-x-5">
+                        <TouchableOpacity
+                            style={styles.bg}
+                            className="p-3 rounded-lg flex-row items-center justify-center w-fit"
+                            onPress={() => navigation.navigate("Chỉnh sửa thông tin", { user: user })}
+                        >
+                            <Feather name="edit" size={24} color={"white"} />
+                            <Text className="w-fit ml-3 font-bold text-white">Chỉnh sửa thông tin</Text>
+                        </TouchableOpacity>
+                        {/* <TouchableOpacity style={styles.bg} className="p-3 rounded-lg flex-row items-center justify-center w-40">
                         <Feather name="eye" size={24} color={"white"} />
                         <Text className="w-20 ml-3 font-bold text-white">Xem trang cá nhân</Text>
                     </TouchableOpacity> */}
+                    </View>
+                    <View className="mt-5 pt-2 px-4 border-t border-gray-300">
+                        <View className="flex-row items-center border-b border-gray-300 py-2">
+                            <Ionicons name="mail" size={24} color="#aaa" />
+                            <Text className="ml-5 text-base">{user.email}</Text>
+                        </View>
+                        <View className="flex-row items-center pl-1 border-b border-gray-300 py-2">
+                            <FontAwesome name="phone" size={24} color="#aaa" />
+                            <Text className="ml-5 text-base">{user.phone}</Text>
+                        </View>
+                        <View className="flex-row items-center pt-2">
+                            <Ionicons name="ios-location-sharp" size={24} color="#aaa" />
+                            <Text className="ml-5 text-base">{user.address}</Text>
+                        </View>
+                    </View>
                 </View>
-                <View className="mt-5 pt-2 px-4 border-t border-gray-300">
-                    <View className="flex-row items-center border-b border-gray-300 py-2">
-                        <Ionicons name="mail" size={24} color="#aaa" />
-                        <Text className="ml-5 text-base">{user.email}</Text>
-                    </View>
-                    <View className="flex-row items-center pl-1 border-b border-gray-300 py-2">
-                        <FontAwesome name="phone" size={24} color="#aaa" />
-                        <Text className="ml-5 text-base">{user.phone}</Text>
-                    </View>
-                    <View className="flex-row items-center pt-2">
-                        <Ionicons name="ios-location-sharp" size={24} color="#aaa" />
-                        <Text className="ml-5 text-base">{user.address}</Text>
-                    </View>
+                <View className="top-28 p-1.5 w-56 h-56 rounded-full absolute items-center justify-center bg-white border-2 border-gray-50">
+                    <Image
+                        src={user.avatar || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYOgVxLPZQlTUfG5XDL-uaQqJ03S3XEMx4xg&usqp=CAU"}
+                        className="w-full h-full object-cover rounded-full"
+                    />
+                </View>
+
+                <View className="border-t border-slate-300 w-screen items-center">
+                    {
+                        auth?.user.role === 'PATIENT' &&
+                        <>
+                            <TouchableOpacity
+                                className="w-screen items-center"
+                                onPress={() => navigation.navigate("Hồ sơ", { onlyShow: true })}
+                            >
+                                <Item title="Hồ sơ khám bệnh" />
+                            </TouchableOpacity>
+                            <TouchableOpacity className="w-screen items-center"
+                                onPress={() => { navigation.navigate("Xem lịch khám") }}
+                            >
+                                <Item title="Lịch khám của tôi" />
+                            </TouchableOpacity>
+                        </>
+                    }
+
+                    {
+                        auth?.user.role === 'DOCTOR' &&
+                        <>
+                            <TouchableOpacity
+                                className="w-screen items-center"
+                                onPress={() => { navigation.navigate("Danh sách bệnh viện") }}
+                            >
+                                <Item title="Danh sách bệnh viện" />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                className="w-screen items-center"
+                                onPress={() => { navigation.navigate("Danh sách bác sĩ") }}
+                            >
+                                <Item title="Danh sách bác sĩ" />
+                            </TouchableOpacity>
+                        </>
+                    }
+
+                    {
+                        auth?.user.role === 'HOSPITAL' &&
+                        <>
+                            <TouchableOpacity className="w-screen items-center"
+                                onPress={() => { navigation.navigate("department-management") }}
+                            >
+                                <Item title="Quản lý khoa" />
+                            </TouchableOpacity>
+                        </>
+                    }
+
+                    <TouchableOpacity
+                        className="w-screen items-center"
+                        onPress={() => { navigation.push("Đổi mật khẩu") }}
+                    >
+                        <Item title="Đổi mật khẩu" />
+                    </TouchableOpacity>
+                    <TouchableOpacity className="w-screen items-center" onPress={logout}>
+                        <Item title="Đăng xuất" />
+                    </TouchableOpacity>
                 </View>
             </View>
-            <View className="top-28 p-1.5 w-56 h-56 rounded-full absolute items-center justify-center bg-white border-2 border-gray-50">
-                <Image
-                    src={user.avatar || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYOgVxLPZQlTUfG5XDL-uaQqJ03S3XEMx4xg&usqp=CAU"}
-                    className="w-full h-full object-cover rounded-full"
-                />
-            </View>
-
-            <View className="border-t border-slate-300 w-screen items-center">
-                {
-                    auth?.user.role === 'PATIENT' &&
-                    <>
-                        <TouchableOpacity
-                            className="w-screen items-center"
-                            onPress={() => navigation.navigate("Hồ sơ", { onlyShow: true })}
-                        >
-                            <Item title="Hồ sơ khám bệnh" />
-                        </TouchableOpacity>
-                        <TouchableOpacity className="w-screen items-center"
-                            onPress={() => { navigation.navigate("Xem lịch khám") }}
-                        >
-                            <Item title="Lịch khám của tôi" />
-                        </TouchableOpacity>
-                    </>
-                }
-
-                {
-                    auth?.user.role === 'DOCTOR' &&
-                    <>
-                        <TouchableOpacity
-                            className="w-screen items-center"
-                            onPress={() => { navigation.navigate("Danh sách bệnh viện") }}
-                        >
-                            <Item title="Danh sách bệnh viện" />
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            className="w-screen items-center"
-                            onPress={() => { navigation.navigate("Danh sách bác sĩ") }}
-                        >
-                            <Item title="Danh sách bác sĩ" />
-                        </TouchableOpacity>
-                    </>
-                }
-
-                {
-                    auth?.user.role === 'HOSPITAL' &&
-                    <>
-                        <TouchableOpacity className="w-screen items-center"
-                            onPress={() => { navigation.navigate("department-management") }}
-                        >
-                            <Item title="Quản lý khoa" />
-                        </TouchableOpacity>
-                    </>
-                }
-
-                <TouchableOpacity
-                    className="w-screen items-center"
-                    onPress={() => { navigation.push("Đổi mật khẩu") }}
-                >
-                    <Item title="Đổi mật khẩu" />
-                </TouchableOpacity>
-                <TouchableOpacity className="w-screen items-center" onPress={logout}>
-                    <Item title="Đăng xuất" />
-                </TouchableOpacity>
-            </View>
-        </View>
+        </ScrollView>
     )
 }
 
